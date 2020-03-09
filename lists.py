87662df188701bb57b71ce20243d441ebbf7dcfe
joffitzer <joffitzer@gmail.com>
2020-03-09 10:09:17 -0400

    simple list functions

---
+++ lists.py
@@ -0,0 +1,25 @@
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+print(friends[0])
+
+print(friends[1:])
+
+print(friends[1:3])
+
+friends[1] = "Mike"
+
+print(friends[1])
+
+friends.append("Creed")
+
+print(friends)
+
+friends.insert(1, "Kelly")
+
+print(friends)
+
+friends.remove("Toby")
+
+print(friends)
+
+print(friends.index("Mike"))
